f05f8f05a5bb9881802d677b8cd2c9d6bf9ddda0
heyaohua <hyh2017312@gmail.com>
2017-07-27 17:51:15 +0800

    First contact with app

--- src/app/questions/answer-detail/answer-detail.component.ts
+++ src/app/questions/answer-detail/answer-detail.component.ts
@@ -25,7 +25,7 @@ export class AnswerDetailComponent implements OnInit {
 
   ngOnInit():void {
 
-    window.alert(window['testAndroid']);
+    window.alert(window['WebAppInterface']['testAndroid']);
     this.route.paramMap
       .switchMap((params: ParamMap) =>{
         return this.questionsService.getAnswerDetail(+params.get('id'))
