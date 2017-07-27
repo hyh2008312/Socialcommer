78b00f108665701282edd525c6b6a3cebb8aeb58
heyaohua <hyh2017312@gmail.com>
2017-07-27 18:43:08 +0800

    First contact with app

--- src/app/components/user-follow/user-follow.service.ts
+++ src/app/components/user-follow/user-follow.service.ts
@@ -14,6 +14,7 @@ export class FollowService {
   createAuthorizationHeader(headers: Headers) {
 
     if(window['WebAppInterface']) {
+      window.alert(window['WebAppInterface'].getAccessToken());
       headers.append('Authorization', window['WebAppInterface'].getAccessToken() != '' ?
       'Bearer ' + window['WebAppInterface'].getAccessToken(): '');
     }
