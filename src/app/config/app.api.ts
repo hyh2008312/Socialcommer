24191f2b4018fa1cefecf6c1a0d15fc8fc3c8b9e
heyaohua <hyh2017312@gmail.com>
2017-08-02 15:17:55 +0800

    Deploy

--- src/app/config/app.api.ts
+++ src/app/config/app.api.ts
@@ -1,11 +1,11 @@
-import { environment } from '../../environments/environment';
+declare const process: any; // Typescript compiler will complain without this
 
 export class BaseApi{
   url: string;
 
   constructor(){
     this.url = 'https://api-staging.xberts.com/';
-    if(environment.env === 'prod') {
+    if(process.env.ENV === 'prod') {
       this.url = 'https://api.xberts.com/';
     }
 
