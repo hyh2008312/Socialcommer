ec4e6081436a1a689bc8f87de627a87b08829e46
heyaohua <hyh2017312@gmail.com>
2017-08-02 15:55:24 +0800

    FirstDeploy

--- src/app/config/app.api.ts
+++ src/app/config/app.api.ts
@@ -1,11 +1,9 @@
-declare const process: any; // Typescript compiler will complain without this
-
 export class BaseApi{
   url: string;
 
   constructor(){
     this.url = 'https://api-staging.xberts.com/';
-    if(process.env.ENV === 'prod') {
+    if(window.env.ENV === 'prod') {
       this.url = 'https://api.xberts.com/';
     }
 
