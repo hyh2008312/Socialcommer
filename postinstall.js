7c248dcc8d773f54c0883d3d5b2d68cadaa9450b
heyaohua <hyh2017312@gmail.com>
2017-08-02 16:31:42 +0800

    FirstDeploy

--- postinstall.js
+++
@@ -1,9 +0,0 @@
-const TARGET = process.env.ENV;
-
-if (TARGET === 'dev') {
-  console.dir(`ng build --prod --env=dev -aot`);
-}
-
-if (TARGET === 'prod') {
-  console.dir(`ng build --prod --env=prod -aot`);
-}
