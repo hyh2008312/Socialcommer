380eeb48755153e6ddc6b2c95761332ff4d4dd2d
heyaohua <hyh2017312@gmail.com>
2017-08-02 16:06:13 +0800

    FirstDeploy

--- src/app/config/app.api.ts
+++ src/app/config/app.api.ts
@@ -1,9 +1,15 @@
+import { environment } from '../../environments/environment';
+
 export class BaseApi{
   url: string;
 
   constructor(){
     this.url = 'https://api-staging.xberts.com/';
-    if((<any>window).env.ENV === 'prod') {
+    if((<any>window).env == 'prod') {
+      this.url = 'https://api.xberts.com/';
+      return;
+    }
+    if(environment.production === true) {
       this.url = 'https://api.xberts.com/';
     }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: false,
-  env: 'dev'
+  production: false
 };
