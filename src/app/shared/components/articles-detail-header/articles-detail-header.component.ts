9ead8f5e5a4983498780e9c7a046293126a727d8
heyaohua <hyh2017312@gmail.com>
2017-09-15 19:21:53 +0800

    [Ask]:correct the ask main page display

--- src/app/shared/components/articles-detail-header/articles-detail-header.component.ts
+++ src/app/shared/components/articles-detail-header/articles-detail-header.component.ts
@@ -38,7 +38,7 @@ export class ArticlesDetailHeaderComponent implements OnInit {
 
   ngOnChanges(): void{
     if(this.userId != '' && this.interactId) {
-      aler(this.userId);
+      alert(this.userId);
       let self = this;
 
       self.articlesDetailHeaderService.getJoiner(self.interactId,self.userId).then(Joiners => {
