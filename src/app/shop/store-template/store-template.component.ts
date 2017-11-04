4f43f8c180f1cb9c95935ab4469cf8eeb8ab819d
heyh <hyh2017312@gmail.com>
2017-11-04 13:12:29 +0800

    [Shop]:[StoreTemplate]:add store template preview

--- src/app/shop/store-template/store-template.component.ts
+++ src/app/shop/store-template/store-template.component.ts
@@ -31,6 +31,49 @@ export class StoreTemplateComponent implements OnInit {
   public shareLink: string;
   public text = '';
   user: User = new User();
+  product = [{
+    imageUrl : "https://media.xberts.com/collector/source/web/templats/01-pic-1.jpg",
+    originalPriceAmount : 55.95,
+    originalPriceCurrency : "USD",
+    salePriceAmount : 39.30,
+    salePriceCurrency : "USD",
+    title : "Skin Care and Cosmetic Ingredients Dictionary. "
+  }, {
+    imageUrl : "https://media.xberts.com/collector/source/web/templats/01-pic-2.jpg",
+    originalPriceAmount : 39.00,
+    originalPriceCurrency : "USD",
+    salePriceAmount : 19.00,
+    salePriceCurrency : "USD",
+    title : "Mermaid Chunky Glitter Large 30g Jar COSMETIC GLITTER Festival Face Body"
+  },{
+    imageUrl : "https://media.xberts.com/collector/source/web/templats/01-pic-3.jpg",
+    originalPriceAmount : 39.00,
+    originalPriceCurrency : "USD",
+    salePriceAmount : 19.00,
+    salePriceCurrency : "USD",
+    title : "Black Markup Mirror 6 Inch 3x Magnification LED Light Two-Sided Table"
+  },{
+    imageUrl : "https://media.xberts.com/collector/source/web/templats/01-pic-4.jpg",
+    originalPriceAmount : 49.99,
+    originalPriceCurrency : "USD",
+    salePriceAmount : 19.99,
+    salePriceCurrency : "USD",
+    title : "Eyelash Dreamer Makeup Bag, Eyelash Dreamer, Makeup Bag, Makeup, Lash "
+  },{
+    imageUrl : "https://media.xberts.com/collector/source/web/templats/01-pic-5.jpg",
+    originalPriceAmount : 6.00,
+    originalPriceCurrency : "USD",
+    salePriceAmount : 4.99,
+    salePriceCurrency : "USD",
+    title : "E.l.f Hydrating Face Primer, 0.47 Fluid Ounce"
+  },{
+    imageUrl : "https://media.xberts.com/collector/source/web/templats/01-pic-6.jpg",
+    originalPriceAmount : 49.99,
+    originalPriceCurrency : "USD",
+    salePriceAmount : 35.99,
+    salePriceCurrency : "USD",
+    title : "The Best Organic Vitamin C Serum - [BIG 2-OZ Bottle] - Hyaluronic Acid, 20% C + E"
+  }];
 
   constructor(
     private userService: UserService
