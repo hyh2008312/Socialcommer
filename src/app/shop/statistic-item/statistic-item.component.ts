58c459fd69bca1c01aa6b21bb40d9417339579f0
heyh <hyh2017312@gmail.com>
2017-11-09 14:09:19 +0800

    [SocialCommer]:[Login]:correct the login module

---
+++ src/app/shop/statistic-item/statistic-item.component.ts
@@ -0,0 +1,21 @@
+import { Input, Output, Component, OnInit,EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-statistic-item',
+  templateUrl: './statistic-item.component.html',
+  styleUrls: ['../shop.scss']
+})
+
+export class StatisticItemComponent implements OnInit {
+
+  @Input() data: any;
+
+  constructor(
+
+  ) { }
+
+  ngOnInit(): void {
+
+  }
+
+}
