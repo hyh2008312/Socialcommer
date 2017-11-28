e12d5faa3144974728f029887ffd53a0f4f76ced
LuZhenQiang <1972047920@qq.com>
2017-11-28 13:34:22 +0800

    店铺第二套模版

---
+++ src/app/store/store-template-2/store-template-2.module.ts
@@ -0,0 +1,46 @@
+import {NgModule} from '@angular/core';
+import {MainPageComponent} from './main-page/main-page.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {StoreListComponent} from './store-list/store-list.component';
+import {AboutMeComponent} from './about-me/about-me.component';
+import {StoreCategoryComponent} from './store-category/store-category.component';
+import {StoreDetailComponent} from './store-detail/store-detail.component';
+import {StoreItemCardComponent} from './store-item-card/store-item-card.component';
+
+import {StoreNavigationComponent} from './store-navigation/store-navigation.component'
+
+import {StoreTemplateTwoRoutingModule} from './store-template-2.routes.module';
+import {StoreService} from '../store.service';
+
+import {SharedModule} from '../../shared/shared.module';
+import {StoreFootComponent} from './store-foot/store-foot.component';
+import {StoreDetailPictureComponent} from './store-detail-picture/store-detail-picture.component';
+import {StoreListDetailComponent} from './store-detail/store-list-detail.component';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    StoreTemplateTwoRoutingModule
+  ],
+  exports: [],
+  declarations: [
+    MainPageComponent,
+    HomePageComponent,
+    StoreListComponent,
+    AboutMeComponent,
+    StoreCategoryComponent,
+    StoreDetailComponent,
+    StoreItemCardComponent,
+    StoreNavigationComponent,
+    StoreFootComponent,
+    StoreDetailPictureComponent,
+    StoreListDetailComponent
+  ],
+  entryComponents: [],
+  providers: [
+    StoreService
+  ]
+})
+export class StoreTemplateTwoModule {
+}
+
