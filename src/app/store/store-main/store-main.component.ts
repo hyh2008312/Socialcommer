57f33223d3fad8c64855694f17c49dcbbcb91246
heyh <hyh2017312@gmail.com>
2017-12-05 16:15:35 +0800

    [SocialCommer]:correct the socialCommer content bugs

--- src/app/store/store-main/store-main.component.ts
+++ src/app/store/store-main/store-main.component.ts
@@ -30,20 +30,44 @@ export class StoreMainComponent implements OnInit {
   ngOnInit():void {
     let storeName = this.activatedRoute.snapshot.params['name'];
     let self = this;
-    this.storeService.getStore(storeName).then((data) => {
-      if(data) {
-        this.storeService.addStore(data);
-        if(data.templateId == 1) {
-          window['templateType'] = 1;
-          self.router.navigate([`/store/${storeName}/${data.templateId}`]);
-          return;
-        }
-        if(data.uid > 1) {
-          window['templateType'] = data.uid;
-          self.router.navigate([`/store/${storeName}/${data.uid}`]);
-        }
+    let routerArray = this.router.url.split(storeName);
+    if(routerArray[1] != '') {
+      let allRouter = routerArray[1].split('/');
+      let uid = allRouter[1];
+      let lastRouter = '';
+      if(allRouter.length > 2) {
+        lastRouter = routerArray[1].split('/' + uid)[1];
       }
-    });
+      this.storeService.getStore(storeName).then((data) => {
+        if (data) {
+          this.storeService.addStore(data);
+          if (data.templateId == 1) {
+            if(uid != data.templateId) {
+              self.router.navigate([`/store/${storeName}/${data.templateId}${lastRouter}`]);
+            }
+            return;
+          }
+          if(data.uid > 1) {
+            if(uid != data.uid) {
+              self.router.navigate([`/store/${storeName}/${data.uid}${lastRouter}`]);
+            }
+          }
+        }
+      });
+    } else {
+      this.storeService.getStore(storeName).then((data) => {
+        if (data) {
+          this.storeService.addStore(data);
+          if (data.templateId == 1) {
+            self.router.navigate([`/store/${storeName}/${data.templateId}`]);
+            return;
+          }
+          if (data.uid > 1) {
+            self.router.navigate([`/store/${storeName}/${data.uid}`]);
+          }
+        }
+      });
+    }
 
   }
 
