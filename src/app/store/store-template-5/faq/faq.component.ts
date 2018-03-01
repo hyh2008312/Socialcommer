9915562b8291cbebf82d501bdcc10eee2c048edd
LuZhenQiang <1972047920@qq.com>
2018-03-01 19:38:23 +0800

    第五套店铺模版的底部导航

---
+++ src/app/store/store-template-5/faq/faq.component.ts
@@ -0,0 +1,53 @@
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+
+import {StoreService} from '../../store.service';
+import {Store} from '../../store';
+
+@Component({
+  selector: 'app-shop-template-5-about',
+  templateUrl: './faq.component.html',
+  styleUrls: ['../store-template-5.scss']
+})
+
+export class FaqComponent implements OnInit {
+  public shareLink: string;
+  public text = '';
+  store: Store = new Store();
+  contextList: any = {};
+  ownerFirstName: string;
+  ownerLastName: string;
+
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private storeService: StoreService) {
+
+  }
+
+  ngOnInit(): void {
+    this.shareLink = window.location.href;
+
+    let self = this;
+
+    this.storeService.store.subscribe((data) => {
+      if (data) {
+        self.store = data;
+        self.contextList = data.context ? data.context : {};
+        self.text = data.description;
+        self.ownerFirstName = data.ownerFirstName;
+        self.ownerLastName = data.ownerLastName;
+        self.storeService.addTitleDescription({
+          title: data.name,
+          description: data.description,
+          shareImage: data.imageUrl
+        });
+        self.storeService.pageView({
+          pageType: 'store',
+          viewTime: new Date().getTime(),
+          storeId: data.id
+        });
+
+      }
+    });
+  }
+}
