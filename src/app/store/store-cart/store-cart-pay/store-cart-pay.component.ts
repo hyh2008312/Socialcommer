ed4202d055a71caff55c14feac1334dc038dd366
heyh <hyh2017312@gmail.com>
2018-03-12 10:06:17 +0800

    [Store]:[StoreCart]:correct the store cart pay

--- src/app/store/store-cart/store-cart-pay/store-cart-pay.component.ts
+++ src/app/store/store-cart/store-cart-pay/store-cart-pay.component.ts
@@ -600,7 +600,7 @@ export class StoreCartPayComponent implements OnInit{
           order.payerID = data.payerID;
           order.paymentID = data.paymentID;
           order.paymentToken = data.paymentToken;
-          order.orderId = self.order.id;
+          order.orderId = self.order.number;
           order.paymentAmount = self.totalPrice;
           order.paymentCurrency = self.order.currency;
 
@@ -672,7 +672,7 @@ export class StoreCartPayComponent implements OnInit{
           order.payerID = data.payerID;
           order.paymentID = data.paymentID;
           order.paymentToken = data.paymentToken;
-          order.orderId = self.order.id;
+          order.orderId = self.order.number;
           order.paymentAmount = self.totalPrice;
           order.paymentCurrency = self.order.currency;
 
