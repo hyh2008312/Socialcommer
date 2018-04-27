7ff3d4ad1d80430228eb3a03cbc04a7cd83f15c7
hyh2008312 <hyh2008312@163.com>
2018-04-27 19:57:55 +0800

    [Shop]:[Landing]:correct shop landing

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../shared/services/user/user.service';
 
-import { SwiperComponent, SwiperDirective, SwiperConfigInterface,
-  SwiperScrollbarInterface, SwiperPaginationInterface } from 'ngx-swiper-wrapper';
+import {
+  SwiperComponent, SwiperDirective, SwiperConfigInterface
+} from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-landing-page',
@@ -12,6 +13,117 @@ import { SwiperComponent, SwiperDirective, SwiperConfigInterface,
 })
 
 export class LandingPageComponent implements OnInit {
+
+  slideIndex: number = 1;
+
+  public show: boolean = true;
+
+  public type: string = 'component';
+
+  public slides = [{
+    slideImage: 'https://media.socialcommer.com/source/web/pic/landing-group-20180426.png',
+    image: 'https://media.socialcommer.com/source/web/pic/landing-group2-20180426.png',
+    title: 'Maryam Hampton\'s Collections',
+    content: 'Make your life look way more glamorous than it really is',
+    avatar: 'https://media.socialcommer.com/source/web/pic/landing-group-20180426.png',
+    name: 'Maria Brural',
+    url: 'https://www.socialcommer.com/store/maryamhampton/6'
+  }, {
+    slideImage: 'https://media.socialcommer.com/source/web/pic/landing-group-20180426.png',
+    image: 'https://media.socialcommer.com/source/web/pic/landing-group2-20180426.png',
+    title: 'Maryam Hampton\'s Collections',
+    content: 'Make your life look way more glamorous than it really is',
+    avatar: 'https://media.socialcommer.com/source/web/pic/landing-group-20180426.png',
+    name: 'Maria Brural',
+    url: 'https://www.socialcommer.com/store/maryamhampton/6'
+  }];
+
+  public slides1 = [{
+    image: 'https://media.socialcommer.com/cdn/product/cc/5e7ed89d-f3c5-4063-9e63-b68b990f0530.jpg',
+    title: 'SAM EDELMAN Sable suede over-the-knee boots',
+    salePrice: '$ 15.00 ',
+    originalPrice: '$ 10.00 ',
+    commission: '20.0'
+  }, {
+    image: 'https://media.socialcommer.com/cdn/product/cc/5e7ed89d-f3c5-4063-9e63-b68b990f0530.jpg',
+    title: 'SAM EDELMAN Sable suede over-the-knee boots',
+    salePrice: '$ 15.00 ',
+    originalPrice: '$ 10.00 ',
+    commission: '20.0'
+  }, {
+    image: 'https://media.socialcommer.com/cdn/product/cc/5e7ed89d-f3c5-4063-9e63-b68b990f0530.jpg',
+    title: 'SAM EDELMAN Sable suede over-the-knee boots',
+    salePrice: '$ 15.00 ',
+    originalPrice: '$ 10.00 ',
+    commission: '20.0'
+  }, {
+    image: 'https://media.socialcommer.com/cdn/product/cc/5e7ed89d-f3c5-4063-9e63-b68b990f0530.jpg',
+    title: 'SAM EDELMAN Sable suede over-the-knee boots',
+    salePrice: '$ 15.00 ',
+    originalPrice: '$ 10.00 ',
+    commission: '20.0'
+  }, {
+    image: 'https://media.socialcommer.com/cdn/product/cc/5e7ed89d-f3c5-4063-9e63-b68b990f0530.jpg',
+    title: 'SAM EDELMAN Sable suede over-the-knee boots',
+    salePrice: '$ 15.00 ',
+    originalPrice: '$ 10.00 ',
+    commission: '20.0'
+  }, {
+    image: 'https://media.socialcommer.com/cdn/product/cc/5e7ed89d-f3c5-4063-9e63-b68b990f0530.jpg',
+    title: 'SAM EDELMAN Sable suede over-the-knee boots',
+    salePrice: '$ 15.00 ',
+    originalPrice: '$ 10.00 ',
+    commission: '20.0'
+  }, {
+    image: 'https://media.socialcommer.com/cdn/product/cc/5e7ed89d-f3c5-4063-9e63-b68b990f0530.jpg',
+    title: 'SAM EDELMAN Sable suede over-the-knee boots',
+    salePrice: '$ 15.00 ',
+    originalPrice: '$ 10.00 ',
+    commission: '20.0'
+  }];
+
+  public config: SwiperConfigInterface = {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    speed: 800,
+    loop: true,
+    autoplay: {
+      delay: 10000
+    },
+    keyboard: true,
+    mousewheel: true,
+    scrollbar: false,
+    navigation: false,
+    pagination: false
+  };
+
+  public config1: SwiperConfigInterface = {
+    direction: 'horizontal',
+    slidesPerView: 5,
+    speed: 800,
+    loop: true,
+    autoplay: {
+      delay: 5000
+    },
+    keyboard: true,
+    mousewheel: true,
+    scrollbar: false,
+    navigation: false,
+    pagination: false
+  };
+
+  public config2: SwiperConfigInterface = {
+    direction: 'horizontal',
+    slidesPerView: 3,
+    speed: 800,
+    loop: true,
+    autoplay: false,
+    keyboard: true,
+    mousewheel: true,
+    scrollbar: false,
+    navigation: false,
+    pagination: false
+  };
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -45,42 +157,20 @@ export class LandingPageComponent implements OnInit {
     });
   }
 
-  public slides = [
-    'First slide',
-    'Second slide',
-    'Third slide',
-    'Fourth slide',
-    'Fifth slide',
-    'Sixth slide'
-  ];
-
-  public config: SwiperConfigInterface = {
-    direction: 'horizontal',
-    slidesPerView: 1,
-    keyboard: true,
-    mousewheel: true,
-    scrollbar: false,
-    navigation: true,
-    pagination: false
-  };
-
-  private scrollbar: SwiperScrollbarInterface = {
-    el: '.swiper-scrollbar',
-    hide: false,
-    draggable: true
-  };
-
-  private pagination: SwiperPaginationInterface = {
-    el: '.swiper-pagination',
-    clickable: true,
-    hideOnClick: false
-  };
-
   @ViewChild(SwiperComponent) componentRef: SwiperComponent;
   @ViewChild(SwiperDirective) directiveRef: SwiperDirective;
 
   public onIndexChange(index: number): void {
-    console.log('Swiper index: ', index);
+    this.slideIndex = index;
+  }
+
+  changeIndex(index) {
+    this.slideIndex = index;
+    this.componentRef.directiveRef.setIndex(this.slideIndex);
+  }
+
+  public onIndexChange1(index: number): void {
+
   }
 
 
